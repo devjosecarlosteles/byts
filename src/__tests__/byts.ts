b8d2eb6c4958793e49e1527129a082972c39200c
devjosecarlosteles <devjosecarlosteles@gmail.com>
2022-10-01 11:08:43 -0300

    feat: rename tests

--- src/__tests__/byts.ts
+++ src/__tests__/byts.ts
@@ -1,19 +1,19 @@
-import byts from '../../byts';
+import toths from '../../toths';
 
 import messagesErrorsOfEnv from "../errors/env.json";
 
 describe("testing environment module", () => {
   it("should return void", () => {
-    expect(byts.env.set("username", "devjo")).toBeUndefined();
+    expect(toths.env.set("username", "devjo")).toBeUndefined();
   });
 
   it("should return value of environment username", () => {
-    expect(byts.env.get("username")).toBe("devjo");
+    expect(toths.env.get("username")).toBe("devjo");
   })
 
   it("should test if set env will return an error if it sends an empty string as the first parameter", () => {
     try {
-      byts.env.set("", "value");
+      toths.env.set("", "value");
     } catch (err: any) {
       expect(err.message).toBe(messagesErrorsOfEnv.emptyString);
     }
